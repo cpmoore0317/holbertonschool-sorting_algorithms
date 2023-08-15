1496e323bcc7a78ba3182ba1e1958598c653350e
“cpmoore0317” <kingpkayy@gmail.com>
2023-08-14 19:31:12 -0700

    Adding prototype for selection_sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -8,7 +8,7 @@
  * Return: Void
  */
 
-void selecttion_sort(int *array, size_t size)
+void selection_sort(int *array, size_t size)
 {
 	size_t i, j, min_idx;
 	int temp;
@@ -18,7 +18,7 @@ void selecttion_sort(int *array, size_t size)
 		min_idx = i;
 		for (j = i + 1; j < size; j++)
 		{
-			if (array[j] < array[mid_idx])
+			if (array[j] < array[min_idx])
 				min_idx = j;
 		}
 		if (min_idx != i)
